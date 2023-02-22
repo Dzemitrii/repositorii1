f36b9bc1f9881a7f099752985b4852eb5bf0f182
20390 <20390@1k129s5.dune.edu>
2023-02-22 10:28:46 +0200

    Привет человек

--- src/lb4/Lb4.java
+++ src/lb4/Lb4.java
@@ -2,7 +2,7 @@ package lb4;
 public class Lb4 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        
+        System.out.println("Hello person");
         
         }
     }
