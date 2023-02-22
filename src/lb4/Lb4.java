e62b4f445f8f4e2136fccb3dfac0b4ae606f4568
20390 <20390@1k129s5.dune.edu>
2023-02-22 10:53:42 +0200

    Revert "Привет "
    This reverts commit c9111cea634eb5969ad6cd2dc6694fdc9ecb63fc.

--- src/lb4/Lb4.java
+++ src/lb4/Lb4.java
@@ -3,7 +3,6 @@ public class Lb4 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello person");
-        System.out.println("Hello");
         
         }
     }
